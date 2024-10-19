9ee7f45a0f83a50a51c0e938f9169b35fea749f4
Artem Khazanov <tulenka04@gmail.com>
2024-10-19 08:51:36 +0300

    update methods for company-application

---
+++ src/main/java/telran/view/Menu.java
@@ -0,0 +1,63 @@
+package telran.view;
+
+import java.util.Arrays;
+import java.util.stream.*;
+
+public class Menu implements Item {
+    private String name;
+    private Item[] items;
+    private String symbol = "_";
+    private int nSymbols = 15;
+
+    public Menu(String name, Item[] items) {
+        this.name = name;
+        this.items = Arrays.copyOf(items, items.length);
+    }
+
+    public void setSymbol(String symbol) {
+        this.symbol = symbol;
+    }
+
+    public void setNSymbols(int nSymbols) {
+        this.nSymbols = nSymbols;
+    }
+
+    @Override
+    public String displayName() {
+        return name;
+    }
+
+    @Override
+    public void perform(InputOutput io) {
+        displayTitle(io);
+        Item item = null;
+        boolean running = true;
+        do {
+            displayItems(io);
+            int itemIndex = io.readNumberRange("Select item", "Wrong Item number", 1, items.length).intValue();
+            item = items[itemIndex - 1];
+            try {
+                item.perform(io);
+                running = !item.isExit();
+            } catch (RuntimeException e) {
+                io.writeLine(e.getMessage());
+            }
+        } while (running);
+    }
+
+    private void displayItems(InputOutput io) {
+        IntStream.range(0, items.length).forEach(i -> io.writeLine(String.format("%d. %s", i + 1, items[i].displayName())));
+    }
+
+    private void displayTitle(InputOutput io) {
+        io.writeString(symbol.repeat(nSymbols));
+        io.writeString(displayName());
+        io.writeLine(symbol.repeat(nSymbols));
+    }
+
+    @Override
+    public boolean isExit() {
+        return false;
+    }
+
+}
